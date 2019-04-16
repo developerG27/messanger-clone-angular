5452f9b94a887d1d48abb1921453a96c894ce34b
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-04-16 13:54:50 +0200

    errore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {FormsModule, FormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   operation: string = 'login';
-  constructor() { }
+  email: string = null;
+  password: string = null;
+  constructor(private authenticationService: AuthenticationService) {
+    
+  }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.authenticationService.loginWithEmail(this.email, this.password).then((data) => {
+      alert('login corretto');
+      console.log(data);
+    }).catch((error) => {
+      alert('errore');
+      console.log(error);
+    })
+  }
+
+  register(){
+    this.authenticationService.registerWithEmail(this.email, this.password).then((data) => {
+      alert('Registrato correttamente');
+      console.log(data);
+    }).catch((error) => {
+      alert('errore al momento di registrarsi');
+      console.log(error);
+    })
   }
 
 }
